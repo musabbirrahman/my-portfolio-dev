9985c0d07d5a797da5efface2f598a9ce18fa65e
musabbirrahman <musabbirrahman490@gmail.com>
2026-05-06 20:43:13 +0600

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
